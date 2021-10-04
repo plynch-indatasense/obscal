2c03fe552276446a29cfeac888943bcf5ff6a942
plynch <plynch@foodlogic.com.au>
2021-10-05 07:53:03 +1100

    install command added

---
+++ install.sh
@@ -0,0 +1,14 @@
+#/bin/bash.sh
+mkdir ~/Code
+mkdir ~/.calout
+cd ~/Code
+wget http://hasseg.org/icalBuddy/1.8.8/icalBuddy-v1.8.8.zip
+unzip icalBuddy*
+cd icalBuddy-v1.8.8
+./install.command
+
+
+
+
+
+
